a41d2a410cac6276ce084524a1610e89a19b64c6
lightslash <chambostpol@gmail.com>
2017-01-28 18:15:45 +0100

    Ajout de la classe character

--- characters/character.js
+++ characters/character.js
@@ -1,11 +1,59 @@
 class character {
-	
+
 	constructor(lifePoints, manaPoints, int, strength, agi, luck ){
 		this.lifePoints = lifePoints;
 		this.manaPoints = manaPoints;
 		this.int = int;
 		this.strength = strength;
 		this.agi = agi;
+		this.luck = luck;
+	}
+
+	getLifePoints(){
+		return lifePoints;
+	}
+
+	getManaPoints(){
+		return manaPoints;
+	}
+
+	getInt(){
+		return int;
+	}
+
+	getStrength(){
+		return strength;
+	}
+
+	getAgi(){
+		return agi;
+	}
+
+	getLuck(){
+		return luck;
+	}
+
+	setLifePoints(lifePoints){
+		this.lifePoints = lifePoints;
+	}
+
+	setManaPoints(manaPoints){
+		this.manaPoints = manaPoints;
+	}
+
+	setInt(int){
+		this.int = int;
+	}
+
+	setStrength(strength){
+		this.strength = strength;
+	}
+
+	setAgi(agi){
+		this.agi = agi;
+	}
+
+	setLuck(luck){
 		this.luck = luck;
 	}
 }
---
+++ characters/heroes/human.js
@@ -0,0 +1,5 @@
+class human extends character{
+	constructor(){
+		super.constructor(1000, 100, 10, 10, 10, 10);
+	}
+}
