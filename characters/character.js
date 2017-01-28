11744d446307c3bfb058557c69e77a9f35b637b0
lightslash <chambostpol@gmail.com>
2017-01-28 17:43:59 +0100

    Correction du background

---
+++ characters/character.js
@@ -0,0 +1,11 @@
+class character {
+	
+	constructor(lifePoints, manaPoints, int, strength, agi, luck ){
+		this.lifePoints = lifePoints;
+		this.manaPoints = manaPoints;
+		this.int = int;
+		this.strength = strength;
+		this.agi = agi;
+		this.luck = luck;
+	}
+}
