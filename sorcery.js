068e23db5b6f050b92112b41d3499c9051084d85
Côme LELEU <comeleleu@users.noreply.github.com>
2017-01-30 15:17:01 +0100

    konami code thelastgambit

--- sorcery.js
+++ sorcery.js
@@ -122,4 +122,20 @@ $( function() {
 		//...
 	}
 	
+	//konami code
+	var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+	n = 0;
+	$(document).keydown(function (e) {
+
+	    if (e.keyCode === k[n++]) {
+	        if (n === k.length) {
+	            window.location.replace("http://thelastgambit.comeleleu.fr/");
+	            n = 0;
+	        }
+	    }
+	    else {
+	        n = 0;
+	    }
+	});
+	
 } );
